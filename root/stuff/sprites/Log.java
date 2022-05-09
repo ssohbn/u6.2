3f04254a7ac071c755407e9d2734b3511edddd02
marc :D <92878876+ssohbn@users.noreply.github.com>
2022-05-09 21:24:56 +0000

    idk what im doin

--- root/stuff/sprites/Log.java
+++ root/stuff/sprites/Log.java
@@ -1,3 +1,15 @@
 public class Log extends Sprite {
+	boolean bob = true; 
+
+	/** cute lil bob for the floaty log */
+	private void bob() {
+		if ( this.bob ) {
+			// move up
+
+			} else {
+			// move down
+			
+		}
+	}
 	
 }
