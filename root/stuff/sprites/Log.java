ba41fe8c21b62300b8969f6846c83aa80d7cb541
me <me>
2022-05-11 08:41:00 -0700

    aa

--- root/stuff/sprites/Log.java
+++ root/stuff/sprites/Log.java
@@ -1,4 +1,15 @@
+package root.stuff.sprites;
+
+import root.Sketch;
+import root.stuff.util.Position;
+import root.stuff.util.Size;
+
 public class Log extends Sprite {
+	public Log(Position position, Sketch sketch, Size size) {
+		super(position, sketch, size);
+		//TODO Auto-generated constructor stub
+	}
+
 	boolean bob = true; 
 
 	/** cute lil bob for the floaty log */
