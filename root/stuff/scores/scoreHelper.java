af2d71691223111b4bd2aa7b393c877db2ccabbf
me <me>
2022-05-11 08:35:52 -0700

    idk

--- root/stuff/scores/scoreHelper.java
+++ root/stuff/scores/scoreHelper.java
@@ -2,17 +2,22 @@ package root.stuff.scores;
 import java.io.File;
 import java.io.IOException;
 import java.io.FileWriter;
-
 import java.util.ArrayList;
 
 public class scoreHelper {
-	File init() {
-		return new File("scores.txt");
-
+	static File init() {
+		File myFile = new File("scores.txt");
+		try {
+			myFile.createNewFile();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return myFile;
 	}
+
 	public static void submitScore(int score) {
 		try {
-			FileWriter writer = new FileWriter("scores.txt");
+			FileWriter writer = new FileWriter(init());
 			writer.write(""+score);
 			writer.close();
 			System.out.println("wrote scores");
