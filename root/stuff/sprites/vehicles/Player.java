360208a064a29de08efbb3adf293a8594ec97718
me <me>
2022-05-13 09:34:34 -0700

    player = scarecrow :)

--- root/stuff/sprites/vehicles/Player.java
+++ root/stuff/sprites/vehicles/Player.java
@@ -1,16 +1,23 @@
 package root.stuff.sprites.vehicles;
 
+import processing.core.PImage;
 import root.Sketch;
-import root.stuff.sprites.Sprite;
+import root.stuff.util.Color;
 import root.stuff.util.Position;
-import root.stuff.util.Size;
 
-public class Player extends Sprite {
+public class Player extends Car {
+	PImage image;
+	
 
-	public Player(Position position, Sketch sketch, Size size) {
-		super(position, sketch, size);
-		this.image = sketch.loadImage("./root/resources/player.png"); 
-		this.image.resize(64, 64);
+	public Player(Position pos, Sketch sketch) {
+		super(pos, sketch);
+		this.color = new Color(255, 255, 255);
+		this.image = sketch.loadImage("./root/resources/player.png");
+	}
+
+	@Override
+	public void draw() {
+		this.sketch.image(this.image, this.position.x, this.position.y);
 	}
 	
 }
