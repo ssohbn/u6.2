4c04da88ae90dc5b77c1cb0d11b48901ddf80fa4
me <me>
2022-05-13 08:48:59 -0700

    desrt stuff

---
+++ root/stuff/tiles/Desert/ForestRow.java
@@ -0,0 +1,37 @@
+package root.stuff.tiles.Desert;
+
+import java.util.Random;
+
+import root.Sketch;
+import root.stuff.tiles.templates.TemplateRow;
+import root.stuff.util.Position;
+import root.stuff.util.Size;
+
+public class ForestRow extends TemplateRow {
+
+	@Override
+	protected void genTiles(int y, Sketch sketch) {
+		Random rand = new Random();
+		for (int i = 0; i < this.tiles.length; i++) {
+			Position position = new Position(i * 64, y);
+			int randomNum = rand.nextInt(8);
+			if (randomNum == 1) {
+				tiles[i] = new ForestTileTree(position, sketch, new Cactus(position, sketch));
+			} else if (randomNum == 2) {
+				tiles[i] = new ForestTileTree(position, sketch, new PalmTree(position, sketch));
+			} else {
+				tiles[i] = new ForestTile(new Position(i * 64, y), sketch);
+			}
+		}
+	}
+
+	public ForestRow(int y, Sketch sketch) {
+		super(y, sketch);
+		//TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public Size getSize() {
+		return this.size;
+	}
+}
---
+++ root/stuff/tiles/Desert/ForestTile.java
@@ -0,0 +1,19 @@
+package root.stuff.tiles.Forest;
+
+import root.Sketch;
+import root.stuff.tiles.templates.TemplateTile;
+import root.stuff.util.Position;
+
+public class ForestTile	extends TemplateTile {
+
+	public ForestTile(Position position, Sketch sketch) {
+		super(position, sketch);
+	}
+
+	@Override
+	public void draw() {
+		this.sketch.fill(50, 180, 50);
+		super.draw();
+	}
+
+}
