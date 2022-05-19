f8db6251613c6a1550a298c8cba23ab1553470b4
marc <marc>
2022-05-19 10:50:45 -0700

    hi22

--- root/Sketch.java
+++ root/Sketch.java
@@ -2,6 +2,7 @@ package root;
 
 import processing.core.PApplet;
 import processing.core.PFont;
+import processing.core.PImage;
 import root.stuff.screen.Screen;
 import root.stuff.screen.StartScreen;
 import root.stuff.util.Size;
@@ -10,6 +11,10 @@ public class Sketch extends PApplet {
 
 	public PFont arcade;
 	public PFont score;
+	public PImage CAR1;
+	public PImage CAR2;
+	public PImage CAR3;
+	public PImage HELI;
 
 	public Screen screen = new StartScreen(this);
 	public static final Size SCREEN_SIZE = new Size(640, 640);
@@ -24,6 +29,13 @@ public class Sketch extends PApplet {
 		background(255);
 		arcade = createFont("./root/resources/arcade.TTF", 128);
 		score = createFont("./root/resources/score.otf", 128);
+
+		CAR1 = loadImage("./root/resources/CAR1.png");
+		CAR2 = loadImage("./root/resources/CAR2.png");
+		CAR3 = loadImage("./root/resources/CAR3.png");
+		HELI = loadImage("./root/resources/HELI.png");
+
+
 		textFont(arcade);
 	}
   
